d489d06d0d58de20e3f55fdde3b3f603800436da
JanikCodes <piefke2000@gmx.de>
2020-10-26 18:54:43 +0100

    Init

---
+++ src/main/java/com/janik/magicka/Magicka.java
@@ -0,0 +1,25 @@
+package com.janik.magicka;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class Magicka implements ModInitializer {
+
+    public static final Item BACON_ITEM = new Item(new Item.Settings().group(ItemGroup.MISC).food(new FoodComponent.Builder().hunger(1).saturationModifier(7f).alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.POISON,20*3), 0.1f).build()));
+    public static final Item COOKED_BACON_ITEM = new Item(new Item.Settings().group(ItemGroup.MISC).food(new FoodComponent.Builder().hunger(6).saturationModifier(12f).build()));
+    public static final Item ENDERBOWL_ITEM = new Item(new Item.Settings().group(ItemGroup.MISC).food(new FoodComponent.Builder().hunger(6).saturationModifier(12f).alwaysEdible().build()));
+
+    @Override
+    public void onInitialize() {
+        Registry.register(Registry.ITEM, new Identifier("magick","bacon"), BACON_ITEM);
+        Registry.register(Registry.ITEM, new Identifier("magick","cooked_bacon"), COOKED_BACON_ITEM);
+        Registry.register(Registry.ITEM, new Identifier("magick","enderbowl"), ENDERBOWL_ITEM);
+
+    }
+}
