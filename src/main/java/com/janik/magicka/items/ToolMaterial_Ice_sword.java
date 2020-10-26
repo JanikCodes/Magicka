868095e5e8f904b67d060a74d774f7192f96cef3
JanikCodes <piefke2000@gmx.de>
2020-10-26 20:57:55 +0100

    Created Ice_Sword Configuration class

---
+++ src/main/java/com/janik/magicka/items/ToolMaterial_Ice_sword.java
@@ -0,0 +1,38 @@
+package com.janik.magicka.items;
+
+import com.janik.magicka.Magicka;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+
+public class ToolMaterial_Ice_sword implements ToolMaterial {
+
+    @Override
+    public int getDurability() {
+        return 250;
+    }
+
+    @Override
+    public float getMiningSpeed() {
+        return 2;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return 2.5f;
+    }
+
+    @Override
+    public int getMiningLevel() {
+        return 0;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 16;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems();
+    }
+}
