ddc8f0e7c0010d27aad1c4b9f4a06d48d7c7d6f6
Felix Heß <felix.hess@freenet.de>
2020-10-27 19:49:50 +0100

    Added effect when wearing Ice Helmet

--- src/main/java/com/janik/magicka/mixins/PlayerEntityMixins.java
+++ src/main/java/com/janik/magicka/mixins/PlayerEntityMixins.java
@@ -1,9 +1,33 @@
 package com.janik.magicka.mixins;
 
+import com.janik.magicka.Magicka;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.OnAStickItem;
+import net.minecraft.tag.FluidTags;
+import net.minecraft.tag.Tag;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
-public class PlayerEntityMixins {
+public abstract class PlayerEntityMixins {
+
+
+    @Inject(method = "tick", at=@At("TAIL"))
+    public void onTick(CallbackInfo cbi){
+        PlayerEntity entity = (PlayerEntity)(Object)this;
+        ItemStack itemStack = entity.getEquippedStack(EquipmentSlot.HEAD);
+        if (itemStack.getItem() == Magicka.ICE_HELMET && !entity.isSubmergedIn(FluidTags.WATER)) {
+            entity.addStatusEffect(new StatusEffectInstance(StatusEffects.WATER_BREATHING, 200, 0, false, false, true));
+        }
+    }
 
 }
