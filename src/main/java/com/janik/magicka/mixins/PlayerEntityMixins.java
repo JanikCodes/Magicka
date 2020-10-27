29926b0e30b0e1a13aa8e2d3a4dd7e54108634a9
Felix Heß <felix.hess@freenet.de>
2020-10-27 19:21:27 +0100

    Added PlayerEntity Mixin

---
+++ src/main/java/com/janik/magicka/mixins/PlayerEntityMixins.java
@@ -0,0 +1,9 @@
+package com.janik.magicka.mixins;
+
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(PlayerEntity.class)
+public class PlayerEntityMixins {
+
+}
